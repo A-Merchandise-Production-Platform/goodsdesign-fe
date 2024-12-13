da9fa6784eb7e73ea772bdd875ce9379163b20d6
Ngô Gia Huấn <ngogiahuan23122003@gmail.com>
2024-12-13 09:49:52 +0700

    feat(upload): add API for image uploads with multipart/form-data support

--- src/app/(root)/admin/users/page.tsx
+++ src/app/(root)/admin/users/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { UserApi } from '@/api/user';
 import { DynamicAdminHeader } from '@/app/(root)/admin/components/dynamic-admin-header';
 import UserDataTable from '@/app/(root)/admin/users/components/user-data-table';
-import { Button } from '@/components/ui/button';
 
 export default function Page() {
   return (
