326287d287dc8352656c4170bd3b482a2c47d2b6
Ngo Gia Huan <ngogiahuan23122003@gmail.com>
2025-03-22 16:34:56 +0700

    refactor: consolidate profile management components and enhance styling
    - Replaced the AddressUpdate and UpdateProfileForm components with a new ProfileUpdateForm for streamlined profile updates.
    - Updated the profile page layout to utilize the new ProfileUpdateForm, improving user experience.
    - Added a new font configuration for JetBrains Mono in global CSS for better typography.
    - Enhanced calendar component styling and functionality for improved user interaction.
    - Removed deprecated components to simplify the codebase and improve maintainability.

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,28 +1,9 @@
-import AddressUpdate from '@/app/(root)/profile/_component/add-address-button';
-import UpdateProfileForm from '@/app/(root)/profile/_component/update-profile-form';
-import { Separator } from '@/components/ui/separator';
+import ProfileUpdateForm from '@/app/(root)/profile/_component/profile-update-form';
 
 export default function Page() {
   return (
     <div>
-      <div>
-        <h2 className="mb-2 text-xl font-semibold">Profile</h2>
-        <p className="text-muted-foreground mb-10">
-          This is how others will see you on the site.
-        </p>
-      </div>
-
-      <UpdateProfileForm />
-      <Separator className="my-8" />
-      <div className="gap-4q flex items-start">
-        <div className="flex-1">
-          <h2 className="mb-2 text-xl font-semibold">Addresses</h2>
-          <p className="text-muted-foreground">
-            Your saved addresses for faster checkout.
-          </p>
-        </div>
-        <AddressUpdate />
-      </div>
+      <ProfileUpdateForm />
     </div>
   );
 }
