f73f67ff32df48a778f57a3aed09681a03bbbb1e
Ngo Gia Huan <ngogiahuan23122003@gmail.com>
2025-03-27 22:13:37 +0700

    feat: update package.json and package-lock.json for cmdk and sonner versions, add address selector component, and implement address queries in GraphQL

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,6 +1,13 @@
+'use client';
+
 import ProfileUpdateForm from '@/app/(root)/profile/_component/profile-update-form';
+import { useGetAllAddressesQuery } from '@/graphql/generated/graphql';
 
 export default function Page() {
+  const { data, loading } = useGetAllAddressesQuery();
+
+  console.log(data);
+
   return (
     <div>
       <ProfileUpdateForm />
---
+++ src/components/shared/address/address-selector.tsx
@@ -0,0 +1,90 @@
+'use client';
+
+import * as React from 'react';
+import { Check, ChevronsUpDown } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+const frameworks = [
+  {
+    value: 'next.js',
+    label: 'Next.js',
+  },
+  {
+    value: 'sveltekit',
+    label: 'SvelteKit',
+  },
+  {
+    value: 'nuxt.js',
+    label: 'Nuxt.js',
+  },
+  {
+    value: 'remix',
+    label: 'Remix',
+  },
+  {
+    value: 'astro',
+    label: 'Astro',
+  },
+];
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState('');
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find(framework => framework.value === value)?.label
+            : 'Select framework...'}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput />
+          <CommandList>
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>Calendar</CommandItem>
+              <CommandItem>Search</CommandItem>
+              <CommandItem>Settings</CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>Profile</CommandItem>
+              <CommandItem>Billing</CommandItem>
+              <CommandItem>
+                Keyboard Shortcuts
+                <CommandShortcut>⌘K</CommandShortcut>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
